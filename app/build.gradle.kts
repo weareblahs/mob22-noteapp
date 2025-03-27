81baaef8ac89f2fdf69975f5ff78338b61d2d445
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2025-03-27 11:52:32 +0800

    initial commit with configuration, keystore and firestore linking

---
+++ app/build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    kotlin("kapt")
+    id("androidx.navigation.safeargs.kotlin")
+    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.google.gms.google.services)
+}
+
+android {
+    namespace = "com.noteapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.noteapp"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    signingConfigs {
+        // NOTE: sensitive information
+        create("default") {
+            keyAlias = "fcmob"
+            keyPassword = "aaAA11..,," // component
+            storePassword = "aaAA11..,," // file
+            storeFile = file("$projectDir/keystore.jks") // user is required to generate the files by themselves
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("default")
+        }
+
+        debug {
+            isDebuggable = true
+            signingConfig = signingConfigs.getByName("default")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        buildConfig = true;
+        viewBinding = true;
+        dataBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.firestore)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.9")
+//hilt2 = java dependency injector (during build)
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.compiler)
+}
